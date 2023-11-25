4c77395bb64595d6139596133d1f3513fbc5c075
oguzhanoxel <oguzhanoksel@hotmail.com>
2023-11-26 00:49:01 +0300

    repetition execution for event functions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private GameObject _camera;
+
     public float Speed;
 
     private Rigidbody2D _playerRigidbody;
     private Animator _animator;
+    private float _cameraPosZ = -10.0f;
 
     private void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        // _playerRigidbody.velocity = new Vector2(Speed, 0.0f);
     }
 
     private void Update()
@@ -26,7 +34,12 @@ public class PlayerController : MonoBehaviour
             Speed = 0.0f;
         }
 
-        _playerRigidbody.velocity = new Vector2(Speed, 0.0f);
+        transform.position += transform.right * Speed * Time.deltaTime;
         _animator.SetFloat("speed", Speed);
+    }
+
+    private void LateUpdate()
+    {
+        _camera.transform.position = new Vector3(transform.position.x, transform.position.y, _cameraPosZ);
     }
 }
