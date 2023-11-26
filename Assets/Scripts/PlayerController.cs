e0195b856cfae6a281d6f026bbe74964ab362701
oguzhanoxel <oguzhanoksel@hotmail.com>
2023-11-26 22:19:14 +0300

    changed player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,45 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private GameObject _camera;
-
-    public float Speed;
-
+    [SerializeField] private float _jumpForce = 10.0f;
+    [SerializeField] private float _speed = 1.5f;
+    
+    private bool _jump;
+    private bool _grounded;
     private Rigidbody2D _playerRigidbody;
-    private Animator _animator;
-    private float _cameraPosZ = -10.0f;
+    private SpriteRenderer _playerSpriteRenderer;
+    private Animator _playerAnim;
+
+    private void Awake()
+    {
+         _playerAnim = GetComponent<Animator>();
+    }
 
     private void Start()
     {
         _playerRigidbody = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        _playerSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
-        // _playerRigidbody.velocity = new Vector2(Speed, 0.0f);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        _playerRigidbody.velocity = new Vector2(horizontalInput * _speed, _playerRigidbody.velocity.y);
+
+        if(_jump)
+        {
+            _playerRigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _jump = false;
+            _grounded = false;
+            _playerAnim.SetTrigger("jump");
+            _playerAnim.SetBool("grounded", false);
+        }
+
+        _playerAnim.SetBool("grounded", _grounded);
     }
 
     private void Update()
     {
-        if ( Input.GetKey(KeyCode.Space))
+        if (_grounded)
         {
-            Speed = 1.0f;
-        }
-        else
-        {
-            Speed = 0.0f;
-        }
+            if (Input.GetButtonDown("Jump"))
+            {
+                _jump = true;
+            }
 
-        transform.position += transform.right * Speed * Time.deltaTime;
-        _animator.SetFloat("speed", Speed);
+            float horizontalInput = Input.GetAxis("Horizontal");
+            _playerAnim.SetFloat("speed", Mathf.Abs(horizontalInput));
+
+            if (horizontalInput < 0)
+            {
+                _playerSpriteRenderer.flipX = true;
+            } 
+            else if (horizontalInput > 0) 
+            {
+                _playerSpriteRenderer.flipX = false;
+            }
+        }
     }
 
-    private void LateUpdate()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        _camera.transform.position = new Vector3(transform.position.x, transform.position.y, _cameraPosZ);
+        if (collision.gameObject.tag == "Ground")
+        {
+            _grounded = true;
+        }
     }
 }
